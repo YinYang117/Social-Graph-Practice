0672c88ef982ea5afbeb3a9d133856af090e9102
YinYang117 <slcryan2010@yahoo.com>
2021-12-29 15:09:39 -0700

    yata!

---
+++ social.js
@@ -0,0 +1,39 @@
+// Implement the SocialNetwork class here
+class SocialNetwork {
+
+  constructor() {
+    this.users = {};
+    this.follows = {};
+    this.currentID = 0;
+  }
+
+  addUser(name) {
+    this.currentID++
+    let userKey = (this.currentID).toString()
+    this.users[userKey] = {id: this.currentID, name: name}
+    this.follows[userKey] = new Set()
+    return this.currentID
+  }
+
+  getUser(userID) {
+    // Your code here
+  }
+
+  follow(userID1, userID2) {
+    // Your code here
+  }
+
+  getFollows(userID) {
+    // Your code here
+  }
+
+  getFollowers(userID) {
+    // Your code here
+  }
+
+  getRecommendedFollows(userID, degrees) {
+    // Your code here
+  }
+}
+
+module.exports = SocialNetwork;
